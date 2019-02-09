c70e0004248fb919f0c2a65ce3b2ca98cc586907
Jenina Lanzi <jeninalanzi@gmail.com>
2019-02-09 17:45:11 -0600

    most current working draft of project

--- public/js/blog.js
+++ public/js/blog.js
@@ -22,16 +22,15 @@ $(document).ready(function() {
       initializeRows();
     });
   }
-
+  
   // This function calls API to delete reviews
   function deleteReview(id) {
     $.ajax({
       method: "DELETE",
       url: "/api/reviews/" + id
-    })
-    .then(function() {
+    }).then(function() {
       getReviews();
-    })
+    });
   }
 
   // Function that appends all of constructed review HTML inside reviewContainer
@@ -44,37 +43,54 @@ $(document).ready(function() {
 
   // Function that constructs the review HTML!
   function createNewRow(review) {
+    // Create new card object & heading
     var newReviewCard = $("<div>");
     newReviewCard.addClass("card");
     var newReviewCardHeading = $("<div>");
     newReviewCardHeading.addClass("card-header");
+    // Create new objects for edit and delete buttons
     var deleteButton = $("<button>");
-    deleteButton.text("x");
+    deleteButton.text("DELETE");
     deleteButton.addClass("delete-review btn btn-danger");
     var editButton = $("<button>");
     editButton.text("EDIT");
     editButton.addClass("edit-button btn btn-default");
     editButton.attr("data-id", review.id);
+    // Create new objects for review title, author, genre, song title, and artist name
     var newReviewTitle = $("<h2>");
+    var newReviewArtistAndSongTitle = $("<h4>");
     var newReviewGenre = $("<h5>");
     var newReviewAuthor = $("<small>");
+    // Attributing text & css to genre
     newReviewGenre.text(review.genre);
     newReviewGenre.css({
       float: "right",
       "font-weight": "700",
       "margin-top": "-15px"
     });
+    // Attributing text to artist/song
+    newReviewArtistAndSongTitle.text(
+      "Review for: " + review.song + " | " + "by " + review.artist
+    );
+    // Create new object for the card-body (where the REVIEW will go)
     var newReviewCardBody = $("<div>");
     newReviewCardBody.addClass("card-body");
     var newReviewBody = $("<p>");
+    // Adding text values to review title, author, song title, and artist name
     newReviewTitle.text(review.title + " ");
+    // Adding the "review" itself into a usable object
     newReviewBody.text(review.body);
-    newReviewAuthor.text(review.author);
-    newReviewCardHeading.append(deleteButton);
-    newReviewCardHeading.append(editButton);
+    newReviewAuthor.text("Written by: " + review.author);
+    // Appending all data to display on the card-heading
     newReviewCardHeading.append(newReviewTitle);
+    newReviewCardHeading.append(newReviewArtistAndSongTitle);
     newReviewCardHeading.append(newReviewGenre);
+    // Appending the "review" into the card-body
     newReviewCardBody.append(newReviewBody);
+    // Appending the "buttons" after the review body
+    newReviewCardBody.append(editButton);
+    newReviewCardBody.append(deleteButton);
+    // Appending author to the card-body
     newReviewBody.append(newReviewAuthor);
     newReviewCard.append(newReviewCardHeading);
     newReviewCard.append(newReviewCardBody);
@@ -86,9 +102,9 @@ $(document).ready(function() {
   // Function that finds out which review to delete and calls deleteReview
   function handleReviewDelete() {
     var currentReview = $(this)
-    .parent()
-    .parent()
-    .data("review");
+      .parent()
+      .parent()
+      .data("review");
     deleteReview(currentReview.id);
   }
 
@@ -100,16 +116,7 @@ $(document).ready(function() {
     window.location.href = "/cms?review_id=" + currentReview.id;
   }
 
-  // Function displays message when there are NO reviews
-  // function displayEmpty() {
-  //   reviewContainer.empty();
-  //   var messageH2 = $("<h2>");
-  //   messageH2.css({ "text-align": "center", "margin-top": "50px" });
-  //   messageH2.html(
-  //     "There are no reviews yet! Click <a href='/cms'>here</a> to create a new review!"
-  //   );
-  // }
-
   // Getting the initial list of reviews
   getReviews();
+
 });
